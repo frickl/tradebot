a4fc8b1938744f3a973f8d393876da4ec888d102
frickl <frickl@frickl.de>
2025-03-24 10:34:58 +0100

    real trade, api-key, charts, portfolio, api-test

--- botv1.4.py
+++ botv1.4.py
@@ -282,11 +282,10 @@ class ChartWindow(QWidget):
         self.ax.legend()
         self.canvas.draw()
 
+
 # ----------------- Main -----------------
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-
-
